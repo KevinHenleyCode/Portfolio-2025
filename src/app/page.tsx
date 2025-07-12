606db8fdfe36fac40eb15f9df871b17e2fd61f76
Kevin <kevinhenleyinfo@gmail.com>
2025-07-12 16:02:53 -0500

    added link to GitHub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+import { VscGithub } from 'react-icons/vsc'
+
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center p-20'>
@@ -5,6 +8,14 @@ export default function Home() {
       <p className='mt-10 text-xl font-semibold text-lime-400'>
         Setting Up New Portfolio
       </p>
+      <p className='mt-10 text-xl font-medium text-lime-400'>7/12/2025</p>
+      <Link
+        href={'https://github.com/KevinHenleyCode'}
+        target='_blank'
+        className='mt-10 text-4xl text-lime-400 transition-all duration-200 ease-in-out hover:text-lime-600'
+      >
+        <VscGithub />
+      </Link>
     </div>
   )
 }
