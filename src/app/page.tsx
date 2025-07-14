10f4b36c36f48d0774dccd8556e2f2b727e50544
Kevin <kevinhenleyinfo@gmail.com>
2025-07-14 08:12:03 -0500

    updated action to restart PM2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { VscGithub } from 'react-icons/vsc'
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center p-20'>
-      <h2 className='text-6xl'>Kevin Henley Portfolio</h2>
+      <h2 className='text-6xl'>Kevin Henley</h2>
       <p className='mt-10 text-xl font-semibold text-lime-400'>
         Setting Up New Portfolio
       </p>
-      <p className='mt-10 text-xl font-medium text-lime-400'>7/12/2025</p>
+      <p className='mt-10 text-xl font-medium text-lime-400'>7/14/2025</p>
       <Link
         href={'https://github.com/KevinHenleyCode'}
         target='_blank'
