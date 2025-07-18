19312ad2cfdbba5cdf9d7918b7f7ea611e662b82
Kevin <kevinhenleyinfo@gmail.com>
2025-07-18 09:16:07 -0500

    added TechOutline component

--- src/app/tech/page.tsx
+++ src/app/tech/page.tsx
@@ -13,15 +13,97 @@ const Tech = () => {
       <PaddingContainer>
         <div className='mt-20'>
           <TechOutline
-            headerContent={'Rice University Bootcamp:'}
+            firstHeader={'Rice University Bootcamp'}
             summaryContent={
               'In 2021, I completed my Full Stack Bootcamp through Rice University. This program laid the foundation for my development career and introduced me to the MERN stack. It was a crash course in solving real-world problems under tight deadlines, and it gave me the confidence to pursue web development full-time. During this time, I became proficient with:'
             }
-            stackContent={[
-              'Frontend: HTML, CSS, JavaScript, Bootstrap, jQuery',
-              'Backend: Node.js, Express.js',
-              'Databases: MySQL (with Sequelize), MongoDB',
-              'Additional Tools: Git, APIs, PWAs',
+            secondHeader={'Stack'}
+            stacks={[
+              {
+                category: 'Frontend',
+                content: 'HTML, CSS, JavaScript, Bootstrap, jQuery',
+              },
+              { category: 'Backend', content: 'Node.js, Express.js' },
+              {
+                category: 'Databases',
+                content: 'MySQL (with Sequelize), MongoDB',
+              },
+              { category: 'Additional Tools', content: 'Git, APIs, PWAs' },
+            ]}
+          />
+          <TechOutline
+            firstHeader={'VineBrook'}
+            summaryContent={
+              'At VineBrook Homes, I progressed from a junior to a senior developer role, gaining hands-on experience building and leading production-scale applications. I spearheaded the rebuild of our client-facing website and executive reporting platform, transitioning both from CakePHP to a modern stack using Next.js and Express. This cut load times by 50% and significantly reduced code complexity. I also integrated Azure AI tools to streamline internal workflows and boost team efficiency.'
+            }
+            secondHeader={'Stack'}
+            stacks={[
+              {
+                category: 'Frontend',
+                content: 'React.js, Next.js, Tailwind CSS',
+              },
+              { category: 'Backend', content: 'Express.js, REST APIs' },
+              {
+                category: 'Database',
+                content: 'MySQL, SQLite',
+              },
+              {
+                category: 'Tools',
+                content: 'Git, Figma, Adobe, Azure AI services',
+              },
+            ]}
+          />
+          <TechOutline
+            firstHeader={'Currently Using'}
+            summaryContent={
+              'I’m continuing to build and improve my own applications with modern web stacks, focusing on speed, performance, and user experience. My favorite project right now is my Portfolio-2025, although there’s a new favorite about to emerge, which I’ll be outlining over on my Updates Page. Currently, my stack includes:'
+            }
+            secondHeader={'Stack'}
+            stacks={[
+              {
+                category: 'Frontend',
+                content: 'Next.js (v15+), Tailwind CSS, TypeScript',
+              },
+              { category: 'Backend', content: 'Next.js, RESTful APIs' },
+              {
+                category: 'Database',
+                content: 'Prisma ORM with SQLite',
+              },
+              { category: 'Hosting', content: 'AWS EC2' },
+              {
+                category: 'Version Control ',
+                content: 'GitHub, GitHub Actions',
+              },
+              { category: 'Dev Tools', content: 'PM2, NGINX' },
+            ]}
+          />
+          <TechOutline
+            firstHeader={'Currently Learning'}
+            summaryContent={
+              'I’m a firm believer in always staying current. Lately, I’ve been diving into:'
+            }
+            secondHeader={'Content'}
+            stacks={[
+              {
+                category: 'AWS',
+                content:
+                  'For more advanced deployment workflows and scalable app hosting',
+              },
+              {
+                category: 'Rust & Tauri',
+                content: 'Exploring performant desktop app development',
+              },
+              {
+                category: 'WebAssembly (Wasm)',
+                content:
+                  'Looking into ways to bring high-performance native-like features to the web',
+              },
+              {
+                category: 'Offline-first Applications',
+                content:
+                  'Using technologies like Prisma + SQLite + Wasm for PWAs and cross-platform apps',
+              },
+              { category: 'Python', content: 'For scripting, automation' },
             ]}
           />
         </div>
--- src/components/showCasing/TechOutline.tsx
+++ src/components/showCasing/TechOutline.tsx
@@ -1,27 +1,35 @@
 interface TechOutlineProps {
-  headerContent: string
+  firstHeader: string
   summaryContent: string
-  stackContent: string[]
+  secondHeader: string
+  stacks: Array<{
+    category: string
+    content: string
+  }>
 }
 
 const TechOutline = ({
-  headerContent,
+  firstHeader,
   summaryContent,
-  stackContent,
+  secondHeader,
+  stacks,
 }: TechOutlineProps) => {
   return (
     <section className='w-full rounded-2xl border-3 border-rose-600/50 bg-white/10'>
       <h3 className='bg-custom-dark/70 rounded-t-2xl p-2 tracking-wider text-rose-600'>
-        {headerContent}
+        {firstHeader}
       </h3>
       <article className='p-2 text-sm'>{summaryContent}</article>
       <aside className=''>
         <h4 className='bg-custom-dark/70 px-2 py-1 text-lg text-rose-600'>
-          Stack:
+          {secondHeader}:
         </h4>
         <ul className='p-2 text-xs'>
-          {stackContent.map((listItem) => (
-            <li key={listItem}>{listItem}</li>
+          {stacks.map((stack, index) => (
+            <li key={index} className='flex'>
+              <b className='mr-1'>{stack.category}:</b>
+              <p>{stack.content}</p>
+            </li>
           ))}
         </ul>
       </aside>
