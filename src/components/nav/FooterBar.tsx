46a99a00b5fee0c66ca819b09164bd3bd8d5cdb4
Kevin <kevinhenleyinfo@gmail.com>
2025-07-18 10:43:38 -0500

    updated NavBar and FooterBar

--- src/components/nav/FooterBar.tsx
+++ src/components/nav/FooterBar.tsx
@@ -1,16 +1,28 @@
 import Link from 'next/link'
 import { VscGithub } from 'react-icons/vsc'
+import { FaLinkedin } from 'react-icons/fa'
 
 const FooterBar = () => {
   return (
-    <footer className='bg-custom-dark relative w-full px-4 py-2'>
-      <Link
-        href={'https://github.com/KevinHenleyCode'}
-        target='_blank'
-        className='mt-10 text-2xl text-lime-600 transition-all duration-200 ease-in-out hover:text-lime-500'
-      >
-        <VscGithub />
-      </Link>
+    <footer className='bg-custom-dark relative'>
+      <div className='to-custom-dark bg-linear-to-b from-rose-950/40 px-4 py-4'>
+        <span className='mx-auto flex w-1/2 justify-around'>
+          <Link
+            href={'https://github.com/KevinHenleyCode'}
+            target='_blank'
+            className='text-2xl text-lime-600 transition-all duration-200 ease-in-out hover:text-lime-500 lg:text-3xl'
+          >
+            <VscGithub />
+          </Link>
+          <Link
+            href={'https://www.linkedin.com/in/kevin-henley/'}
+            target='_blank'
+            className='text-2xl text-lime-600 transition-all duration-200 ease-in-out hover:text-lime-500 lg:text-3xl'
+          >
+            <FaLinkedin />
+          </Link>
+        </span>
+      </div>
     </footer>
   )
 }
--- src/components/nav/NavLinks.tsx
+++ src/components/nav/NavLinks.tsx
@@ -8,11 +8,16 @@ interface NavLinksProps {
 const NavLinks = ({ links, mobile }: NavLinksProps) => {
   return (
     <ul
-      className={`flex items-center justify-end py-2 ${mobile ? 'flex-col bg-rose-950' : 'h-full flex-row [&>li]:mx-4'}`}
+      className={`flex items-center justify-end py-2 ${mobile ? 'flex-col bg-linear-to-b from-rose-600/60 to-rose-950' : 'h-full flex-row [&>li]:mx-4'}`}
     >
       {links.map((link) => (
         <li key={link}>
-          <Link href={`${link.toLowerCase()}`}>{link}</Link>
+          <Link
+            href={`${link.toLowerCase()}`}
+            className='border-b-2 border-lime-200/0 tracking-widest italic transition-all duration-300 ease-in-out hover:border-lime-200 hover:text-lime-200'
+          >
+            {link}
+          </Link>
         </li>
       ))}
     </ul>
