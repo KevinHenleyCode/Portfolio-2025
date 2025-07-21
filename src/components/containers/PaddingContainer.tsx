439172ab338392fcb9881f608f4633af613f29d7
Kevin <kevinhenleyinfo@gmail.com>
2025-07-20 23:38:06 -0500

    updated padding

--- src/components/containers/PaddingContainer.tsx
+++ src/components/containers/PaddingContainer.tsx
@@ -5,7 +5,7 @@ type PaddingSize = 'sm' | 'md' | 'lg'
 
 const paddingMap: Record<PaddingSize, string> = {
   sm: 'px-2 sm:px-16 xl:px-36', //Needs updating and testing
-  md: 'px-6 sm:px-24 xl:px-48',
+  md: 'px-6 sm:px-24 md:px-36 xl:px-48 2xl:px-60 3xl:px-72 4xl:px-80 5xl:px-96',
   lg: 'px-8 sm:px-36 xl:px-56', //Needs updating and testing
 }
 
--- src/components/textStanards/SubHeader.tsx
+++ src/components/textStanards/SubHeader.tsx
@@ -9,21 +9,21 @@ const SubHeader = ({ content }: SubHeaderProps) => {
     <div className='relative'>
       <span
         aria-hidden='true'
-        className='absolute top-2 z-20 w-full pl-6 sm:pl-24 xl:pl-48'
+        className='3xl:pl-72 4xl:pl-80 5xl:pl-96 absolute top-2 z-20 w-full pl-6 sm:pl-24 md:pl-36 xl:pl-48 2xl:pl-60'
       >
         <span
           aria-hidden='true'
           className='to-custom-dark block h-8 w-full bg-linear-to-r from-lime-100/10 sm:h-9'
         ></span>
       </span>
-      <span className='absolute z-10 mt-2 flex h-8 items-center px-6 sm:h-9 sm:px-24 xl:px-48'>
+      <span className='3xl:pl-72 4xl:pl-80 5xl:pl-96 absolute z-10 mt-2 flex h-8 items-center pl-6 sm:h-9 sm:pl-24 md:pl-36 xl:pl-48 2xl:pl-60'>
         <h2 className='pl-2 text-sm font-semibold text-lime-400 italic sm:text-lg lg:text-xl'>
           {content}
         </h2>
       </span>
       <span
         aria-hidden='true'
-        className='absolute top-2 w-full pl-6 sm:pl-24 xl:pl-48'
+        className='3xl:pl-72 4xl:pl-80 5xl:pl-96 absolute top-2 w-full pl-6 sm:pl-24 md:pl-36 xl:pl-48 2xl:pl-60'
       >
         <span
           aria-hidden='true'
